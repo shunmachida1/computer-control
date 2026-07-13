98230016d0e7fe85e748a06908fa60b0989eb6a1
Shun Machida <shunmachida1@gmail.com>
2026-07-13 13:17:13 -0500

    Fixed scrolling bug

--- src/actions.py
+++ src/actions.py
@@ -1,11 +1,22 @@
 import mediapipe as mp
 import pyautogui
-import utils
+import utils as utils
 
 class ActionDetector:
     def __init__(self):
         self.pinch_down = False
+        self.scrollmode = False
         self.screen_x, self.screen_y = pyautogui.size()
+
+    def move(self, hand):
+        pointer_x = hand.landmark[8].x
+        pointer_x = 1-pointer_x
+        pointer_y = hand.landmark[8].y
+
+        mouse_pos_x = pointer_x * self.screen_x
+        mouse_pos_y = pointer_y * self.screen_y
+
+        pyautogui.moveTo(int(mouse_pos_x), int(mouse_pos_y))
 
     def detect_click(self, hand):
         clickdistance = utils.find_distance(hand, 4, 20)
@@ -18,29 +29,17 @@ class ActionDetector:
 
             else:
                 self.pinch_down = False
-    
-    def move(self, hand):
-        pointer_x = hand.landmark[8].x
-        pointer_x = 1-pointer_x
-        pointer_y = hand.landmark[8].y
-
-        mouse_pos_x = pointer_x * self.screen_x
-        mouse_pos_y = pointer_y * self.screen_y
-
-        pyautogui.moveTo(int(mouse_pos_x), int(mouse_pos_y))
-
-    #Scrolling using middle and thumb modes
 
     def detect_scroll(self, hand):
         scrolldistance = utils.find_distance(hand, 12, 4)
 
         if scrolldistance < 0.08:
-            if not scrollmode:
+            if not self.scrollmode:
                 pyautogui.scroll(-50)
                 print("Scroll")
-                scrollmode = True
+                self.scrollmode = True
 
         else:
-            scrollmode = False
+            self.scrollmode = False
 
     
